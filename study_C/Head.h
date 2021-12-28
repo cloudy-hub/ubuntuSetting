d6ba1c8d4aa0662b87437584b2ebbc9cb5067a07
HuJH <hujianghong@androidex.cn>
2021-12-28 19:21:42 +0800

    修改head.h

--- study_C/Head.h
+++ study_C/Head.h
@@ -1,9 +1,16 @@
-#ifndef __HEAD_H
-#define __HEAD_H
+#ifndef __HEAD_H___
+#define __HEAD_H___
+
+/*********代码执行属性***************/
+#define _SYS_PERIOD     10    // 后台程序执行周期（ms）
+#define _SYS_FREQUENCE       ( 1000 / _SYS_PERIOD )
+#define _SYS_SEC(_n_)     (_SYS_FREQUENCE * (_n_))
 
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "math.h"
+
 
 typedef signed			char int8_t;
 typedef signed short	int int16_t;
@@ -19,17 +26,22 @@ typedef uint8_t		u8;
 typedef uint16_t	u16;
 typedef uint32_t	u32;
 
+/*
+#define null    NULL
 #define false 	0
 #define FALSE 	false
 #define DISABLE false
 #define true 	(!false)
 #define TRUE 	true
 #define ENABLE 	true
+*/
 
-#define BIT_SET(_data_, _bit_)     ((_data_) |= (_bit_))
-#define BIT_CLEAR(_data_, _bit_)   ((_data_) &= ~(_bit_))
+//#error
+
+#define BIT_SET(_data_, _bit_)     do{(_data_) |= (_bit_);}while(0)
+#define BIT_CLEAR(_data_, _bit_)   do{(_data_) &= ~(_bit_);}while(0)
 #define BIT_READ(_data_, _bit_)    ((_data_) & (_bit_))
-#define BIT_INVERT(_data_, _bit_)    ((_data_) & (_bit_))
+#define BIT_INVERT(_data_, _bit_)    do{(_data_) ^= (_bit_);}while(0)
 
 #define _BIN(a,b,c,d,e,f,g,h) ((a<<7)+(b<<6)+(c<<5)+(d<<4)+(e<<3)+(f<<2)+(g<<1)+(h<<0))
 #define _BIT(_bit_)		( 1 << _bit_ )
@@ -37,27 +49,5 @@ typedef uint32_t	u32;
 #define _SIOF_BIT(_char_)   (_SIOF_BYTE(_char_) * 8)
 #define _SIOF_SELF(_char_)  (_SIOF_BYTE(_char_) / _SIOF_BYTE(_char_[0]))
 
-typedef struct 
-{
-	u8 newBtn;
-	u8 oldBtn;
-	u16 holdTime;
-}BTN;
-
-typedef struct 
-{
-	//input
-	BTN Btn;
-	u16 Ticker;
-
-	//contral
-	u8 State;
-
-	//output
-	u8 ErrSet;
-	
-}SYSTEM;
-
-extern SYSTEM sys;
-
 #endif
+
